8f2d0342b443d2c4812f210b4dab99b57400b829
Erick <96069779+Ericsson2004@users.noreply.github.com>
2025-04-14 11:18:40 -0600

    Update services-menu.sh

--- Hands-on 4/services-menu.sh
+++ Hands-on 4/services-menu.sh
@@ -3,7 +3,7 @@
 while true; do
 
     echo "Menu de servicios"
-    echo "1) Listar el contenido de un fichero (carpeta)"
+    echo "1) Listar el contenido de la carpeta"
     echo "2) Crear un archivo de texto con una línea de texto"
     echo "3) Comparar dos archivos de texto"
     echo "4) Mostrar uso de 'awk'"
@@ -14,32 +14,32 @@ while true; do
     case $opcion in
 
         1)
-            read -p "Ingrese la ruta absoluta del directorio: " dir
+            read -p "Ingrese la ruta completa: " dir
             if [ -d "$dir" ]; then
                 ls -l "$dir"
             else
-                echo "Error: El directorio no existe."
+                echo "Error: La carpeta no existe."
             fi
             ;;
         2)
-            read -p "Ingrese el nombre del archivo a crear: " archivo
-            read -p "Ingrese la línea de texto a guardar: " texto
+            read -p "Cuál es nombre del archivo a crear: " archivo
+            read -p "Escriba el texto a guardar: " texto
             archivo_path="$(dirname "$0")/$archivo"
             echo "$texto" > "$archivo_path"
-            echo "Archivo '$archivo' creado con éxito en $(dirname "$0")."
+            echo "Archivo '$archivo' creado de manera correcta en $(dirname "$0")."
             ;;
         3)
             read -p "Ingrese el primer archivo a comparar: " file1
             read -p "Ingrese el segundo archivo a comparar: " file2
 
-            # Asumir que los archivos están en el mismo directorio que el script
+            # Los archivos deben estar en el mismo directorio que el script
             file1_path="$(dirname "$0")/$file1"
             file2_path="$(dirname "$0")/$file2"
 
             if [ -f "$file1_path" ] && [ -f "$file2_path" ]; then
                 diff "$file1_path" "$file2_path"
             else
-                echo "Error: Uno o ambos archivos no existen en $(dirname "$0")."
+                echo "Error: Uno o los dos archivos no existen en $(dirname "$0")."
             fi
             ;;
         4)
